00954ad814ea46fa8cb26eb9311ee35dba337612
Artemy Zhidov <71493037+ortamfag@users.noreply.github.com>
2023-02-23 21:17:08 +0300

    ADD: bad switcher

--- app/js/main.js
+++ app/js/main.js
@@ -13,37 +13,76 @@ const ukr = document.getElementById('ukr');
 const langSwitcher = document.querySelector('.js-nav__lang');
 const languages = document.querySelectorAll('.js-nav__lang--country');
 rus.addEventListener('click', () => {
-  rus.dataset.country = "1";
-  eng.dataset.country = "2";
-  ukr.dataset.country = "3";
+  switch (`${rus.dataset.country}, ${eng.dataset.country}`) {
+    case "1, 2":
+      eng.dataset.country = "1";
+      rus.dataset.country = "2";
+      ukr.dataset.country = "3";
+      break;
+    case "1, 3":
+      eng.dataset.country = "3";
+      rus.dataset.country = "2";
+      ukr.dataset.country = "1";
+      break;
+    case "3, 2":
+      eng.dataset.country = "3";
+      rus.dataset.country = "2";
+      ukr.dataset.country = "1";
+      break;
+    case "3, 1":
+      eng.dataset.country = "1";
+      rus.dataset.country = "2";
+      ukr.dataset.country = "3";
+      break;
+  }
 });
 eng.addEventListener('click', () => {
-  eng.dataset.country = "1";
-  rus.dataset.country = "2";
-  ukr.dataset.country = "3";
+  switch (`${eng.dataset.country}, ${rus.dataset.country}`) {
+    case "1, 2":
+      eng.dataset.country = "2";
+      rus.dataset.country = "1";
+      ukr.dataset.country = "3";
+      break;
+    case "1, 3":
+      eng.dataset.country = "2";
+      rus.dataset.country = "3";
+      ukr.dataset.country = "1";
+      break;
+    case "3, 2":
+      eng.dataset.country = "2";
+      rus.dataset.country = "3";
+      ukr.dataset.country = "1";
+      break;
+    case "3, 1":
+      eng.dataset.country = "2";
+      rus.dataset.country = "1";
+      ukr.dataset.country = "3";
+      break;
+  }
 });
 ukr.addEventListener('click', () => {
-  // if (rus.dataset.country = "1") {
-  //     ukr.dataset.country = "2"
-  //     eng.dataset.country = "3"
-
-  //     setTimeout(() => {
-  //         ukr.dataset.country = "1"
-  //         rus.dataset.country = "2"
-  //     }, 200)
-  // } else {
-  //     ukr.dataset.country = "2"
-  //     rus.dataset.country = "3"
-
-  //     setTimeout(() => {
-  //         ukr.dataset.country = "1"
-  //         eng.dataset.country = "2"
-  //     }, 200)
-  // }
-
-  ukr.dataset.country = "1";
-  rus.dataset.country = "2";
-  eng.dataset.country = "3";
+  switch (`${ukr.dataset.country}, ${eng.dataset.country}`) {
+    case "1, 2":
+      eng.dataset.country = "1";
+      rus.dataset.country = "3";
+      ukr.dataset.country = "2";
+      break;
+    case "1, 3":
+      eng.dataset.country = "3";
+      rus.dataset.country = "1";
+      ukr.dataset.country = "2";
+      break;
+    case "3, 2":
+      eng.dataset.country = "3";
+      rus.dataset.country = "1";
+      ukr.dataset.country = "2";
+      break;
+    case "3, 1":
+      eng.dataset.country = "1";
+      rus.dataset.country = "3";
+      ukr.dataset.country = "2";
+      break;
+  }
 });
 
 /***/ }),
