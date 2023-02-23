6a22b44288150838e152900c4ea168ba433f6ac7
Artemy Zhidov <71493037+ortamfag@users.noreply.github.com>
2023-02-23 15:08:15 +0300

    ADD: switcher animation

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,39 @@
 const rus = document.getElementById('rus');
 const eng = document.getElementById('eng');
 const ukr = document.getElementById('ukr');
+const langSwitcher = document.querySelector('.js-nav__lang');
+const languages = document.querySelectorAll('.js-nav__lang--country');
+rus.addEventListener('click', () => {
+  rus.dataset.country = "1";
+  eng.dataset.country = "2";
+  ukr.dataset.country = "3";
+});
+eng.addEventListener('click', () => {
+  eng.dataset.country = "1";
+  rus.dataset.country = "2";
+  ukr.dataset.country = "3";
+});
+ukr.addEventListener('click', () => {
+  if (rus.dataset.country = "1") {
+    ukr.dataset.country = "2";
+    eng.dataset.country = "3";
+    setTimeout(() => {
+      ukr.dataset.country = "1";
+      rus.dataset.country = "2";
+    }, 100);
+  } else {
+    ukr.dataset.country = "2";
+    rus.dataset.country = "3";
+    setTimeout(() => {
+      ukr.dataset.country = "1";
+      eng.dataset.country = "2";
+    }, 100);
+  }
+
+  // ukr.dataset.country = "1"
+  // rus.dataset.country = "2"
+  // eng.dataset.country = "3"
+});
 
 /***/ }),
 
@@ -26,7 +59,7 @@ __webpack_require__.r(__webpack_exports__);
 
 let lang = navigator.language || navigator.userLanguage;
 let translationEn = {
-  "translate1": "Main"
+  "translate1": "Главн=sdfая"
 };
 let translationRu = {
   "translate1": "Главная"
--- src/js/components/langSwitcher.js
+++ src/js/components/langSwitcher.js
@@ -1,3 +1,42 @@
 const rus = document.getElementById('rus')
 const eng = document.getElementById('eng')
 const ukr = document.getElementById('ukr')
+
+const langSwitcher = document.querySelector('.js-nav__lang')
+const languages = document.querySelectorAll('.js-nav__lang--country')
+
+rus.addEventListener('click', () => {
+    rus.dataset.country = "1"
+    eng.dataset.country = "2"
+    ukr.dataset.country = "3"
+})
+
+eng.addEventListener('click', () => {
+    eng.dataset.country = "1"
+    rus.dataset.country = "2"
+    ukr.dataset.country = "3"
+})
+
+ukr.addEventListener('click', () => {
+    if (rus.dataset.country = "1") {
+        ukr.dataset.country = "2"
+        eng.dataset.country = "3"
+
+        setTimeout(() => {
+            ukr.dataset.country = "1"
+            rus.dataset.country = "2"
+        }, 100)
+    } else {
+        ukr.dataset.country = "2"
+        rus.dataset.country = "3"
+
+        setTimeout(() => {
+            ukr.dataset.country = "1"
+            eng.dataset.country = "2"
+        }, 100)
+    }
+
+    // ukr.dataset.country = "1"
+    // rus.dataset.country = "2"
+    // eng.dataset.country = "3"
+})
