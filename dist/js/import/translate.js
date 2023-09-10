55f04fb9e734ba51feaeb7ccf0dbf60143b32cc1
ortamfag <a_zhidov@outlook.com>
2023-09-10 23:44:21 +0300

    translate script

--- dist/js/import/translate.js
+++ dist/js/import/translate.js
@@ -1,15 +1,29 @@
 import i18next from 'https://cdn.jsdelivr.net/gh/i18next/i18next/src/index.js'
 
-let tranlastionEN = {
-    "title": "Finished design in electronic format",
+let lang = navigator.language || navigator.userLanguage;
+
+window.addEventListener('DOMContentLoaded', () => {
+    if ((lang === 'ru-RU') || (lang === 'kk') || (lang === 'ru')) {
+        lang = 'rus'
+    } else if (lang === 'uk') {
+        lang = 'ua'
+        changeToUA()
+    } else {
+        lang = 'eng'
+        changeToENG()
+    }
+})
+
+let translationEN = {
+    "title": "I develop websites and applications for your business. I observe the structure, I don't overload interfaces with unnecessary decorative elements. By cooperating with me, you get a convenient and beautiful website / application design in the shortest possible time.",
 }
 
-let tranlastionRU = {
-    "title": "Готовый дизайн в электронном формате",
+let translationRU = {
+    "title": "Разрабатываю сайты и приложения для Вашего бизнеса. Соблюдаю структуру, не перегружаю интерфейсы лишними декоративными элементами. Сотрудничая со мной, вы получаете удобный и красивый дизайн сайта/приложения за кратчайшие сроки.",
 }
 
-let tranlastionUA = {
-    "title": "Готовий дизайн в електронному форматі",
+let translationUA = {
+    "title": "Розробляю сайти і додатки для вашого бізнесу. Дотримуюся структуру, не перевантажую інтерфейси зайвими декоративними елементами. Співпрацюючи зі мною, ви отримуєте зручний і красивий дизайн сайту/програми за найкоротші терміни.",
 }
 
 const resources = {
@@ -19,5 +33,35 @@ const resources = {
 
     ru: {
         translation: translationRU
+    },
+
+    ua: {
+        translation: translationUA
     }
 }
+
+i18next.init({
+    resources
+});
+
+const changeToENG = () => {
+    lang = 'eng'
+
+    document.getElementById('title').innerHTML = i18next.t(translationEN.title)
+}
+
+const changeToRU = () => {
+    lang = 'rus'
+
+    document.getElementById('title').innerHTML = i18next.t(translationRU.title)
+}
+
+const changeToUA = () => {
+    lang = 'ua'
+
+    document.getElementById('title').innerHTML = i18next.t(translationUA.title)
+}
+
+document.getElementById('ru').addEventListener('click', () => changeToRU())
+document.getElementById('en').addEventListener('click', () => changeToENG())
+document.getElementById('ua').addEventListener('click', () => changeToUA())
