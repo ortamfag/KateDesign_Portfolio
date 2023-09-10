e9f421ad71c5d453724ab7bfde4cc1257c546af6
ortamfag <a_zhidov@outlook.com>
2023-09-10 19:27:35 +0300

    ADD: start i18n

---
+++ src/js/import/translate.js
@@ -0,0 +1,23 @@
+import i18next from 'https://cdn.jsdelivr.net/gh/i18next/i18next/src/index.js'
+
+let tranlastionEN = {
+    "title": "Finished design in electronic format",
+}
+
+let tranlastionRU = {
+    "title": "Готовый дизайн в электронном формате",
+}
+
+let tranlastionUA = {
+    "title": "Готовий дизайн в електронному форматі",
+}
+
+const resources = {
+    en: {
+        translation: tranlastionEN
+    },
+
+    ru: {
+        translation: tranlastionRU
+    }
+}
